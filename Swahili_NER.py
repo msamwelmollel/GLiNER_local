b32e92f8605e41937864b51e98fe96b0bfa11314
Michael Mollel <47386502+msamwelmollel@users.noreply.github.com>
2024-03-16 03:47:28 +0300

    main

--- Swahili_NER.py
+++ Swahili_NER.py
@@ -1,8 +1,11 @@
+import json
+
+
 from model import GLiNER
 
 # model = GLiNER.from_pretrained("urchade/gliner_base", local_files_only=True)
 
-model = GLiNER.from_pretrained("gliner_mult", local_files_only=True)
+model = GLiNER.from_pretrained("C:/Users/msamwelmollel/GLiNER/gliner_mult/", local_files_only=True)
 
 text = """
 Mimi na baba yangu tunalima mazao mbalimbali toka mwaka 2020
@@ -14,3 +17,10 @@ entities = model.predict_entities(text, labels)
 
 for entity in entities:
     print(entity["text"], "=>", entity["label"])
+
+
+# Parsing JSON string into a Python list
+labels = json.loads(label.json)
+
+# Now 'labels' is a Python list of NER labels.
+labels
